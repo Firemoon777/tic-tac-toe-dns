00bf262db974e13a2597ff3cf3dcaa1ef5cf2cef
Voldemar <firemoon@icloud.com>
2018-08-05 20:26:37 +0300

    read TXT records from file

--- dns.go
+++ dns.go
@@ -7,69 +7,18 @@ import (
 	"strconv"
 
 	"github.com/miekg/dns"
+		"io/ioutil"
 )
 
-var records = map[string]string{
-	"game.f1remoon.com.": "Let's play in tic-tac-toe with dns server!\n" +
-								"Just dig into x.game.f1remoon.com, where x = [0..9]\n" +
-								"7|8|9\n" +
-								"4|5|6\n" +
-								"1|2|3",
-
-	"1.game.f1remoon.com.": "Continue at x.1.game.f1remoon.com!\n" +
-                                "_|_|_\n" + 
-                                "_|o|_\n" + 
-                                "x|_|_",
-
-	"2.game.f1remoon.com.": "Continue at x.2.game.f1remoon.com!\n" +
-                                "_|_|_\n" + 
-                                "_|o|_\n" + 
-                                "_|x|_",
-
-	"3.game.f1remoon.com.": "Continue at x.3.game.f1remoon.com!\n" +
-                                "_|_|_\n" + 
-                                "_|o|_\n" + 
-                                "_|_|x",
-
-	"4.game.f1remoon.com.": "Continue at x.4.game.f1remoon.com!\n" +
-                                "_|_|_\n" + 
-                                "x|o|_\n" + 
-                                "_|_|_",
-
-	"5.game.f1remoon.com.": "Continue at x.5.game.f1remoon.com!\n" +
-                                "o|_|_\n" + 
-                                "_|x|_\n" + 
-                                "_|_|_",
-
-	"6.game.f1remoon.com.": "Continue at x.6.game.f1remoon.com!\n" +
-                                "_|_|_\n" + 
-                                "_|o|x\n" + 
-                                "_|_|_",
-
-	"7.game.f1remoon.com.": "Continue at x.7.game.f1remoon.com!\n" +
-                                "x|_|_\n" + 
-                                "_|o|_\n" + 
-                                "_|_|_",
-
-	"8.game.f1remoon.com.": "Continue at x.8.game.f1remoon.com!\n" +
-                                "_|x|_\n" + 
-                                "_|o|_\n" + 
-                                "_|_|_",
-
-	"9.game.f1remoon.com.": "Continue at x.9.game.f1remoon.com!\n" +
-                                "_|_|x\n" + 
-                                "_|o|_\n" + 
-                                "_|_|_",
-
-}
 func parseQuery(m *dns.Msg) {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeTXT:
 			log.Printf("Query for %s\n", q.Name)
-			ans := records[q.Name];
-			if ans != "" {
-				for _, s := range strings.Split(ans, "\n") {
+			r, err := ioutil.ReadFile("./res/" + q.Name)
+			if err == nil {
+				str := string(r)
+				for _, s := range strings.Split(str, "\n") {
 					t := &dns.TXT{
 						Hdr: dns.RR_Header{Name: q.Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
 						Txt: strings.Split(s, "|"),
